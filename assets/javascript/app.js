8173918fd7d2f23ee662fb8cbaa41398e4a6ca26
jj99999 <jonathan.jackson3@gmail.com>
2016-09-10 13:47:38 -0400

    second js revision

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,6 +6,11 @@
 var searchTerm = "oil";
 var beginDate = "20000101";
 var endDate = "20011231";
+var numResults = 
+
+
+// event lister for the search button
+    $("#search").on('click', function() {
 
 
 // build the queryURL
@@ -14,8 +19,30 @@ var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key
 
 //build ajax call
 $.ajax ({url: queryURL, method: "GET"})
-.done(function(NYTsearch){
+.done(function(response){
 	console.log(response);
 
 
+
+	for (var i = 0; i<numResults; i++) {
+
+		var resultDiv = $("<div>");
+		var pHeadline = $('<h1>').text(response.docs[i].headline.highlight);
+		var pByline = $('<h2>').text(response.docs[i].byline.original);
+		var pSection = $('<h2>').text(response.docs[i].section_name);
+		var pDate = $('<h2>').text(response.docs[i].pub_date);
+		var pURL = $('<h2>').text(response.docs[i].web_url);
+
+		resultDiv.append(pHeadline).append(pByline).append(pSection).append(pDate).append(pURL);
+
+		// NEED to take resultDiv and append into the container div where results appear
+
+	}
+
+
+
+
 })
+
+});
+
