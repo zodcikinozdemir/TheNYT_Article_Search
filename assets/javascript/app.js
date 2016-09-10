db9e88fa9db2bcaf4bb1b9031b705b30df045c10
jj99999 <jonathan.jackson3@gmail.com>
2016-09-10 13:22:45 -0400

    first js push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,21 @@
 // api key    5aa79c02ea5840d28aa684eed86fe61c
 
 
+// variables for the inputs
+
+var searchTerm = "oil";
+var beginDate = "20000101";
+var endDate = "20011231";
+
+
+// build the queryURL
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + 5aa79c02ea5840d28aa684eed86fe61c + "&q=" + searchTerm
++ "&begin_date=" + beginDate + "&end_date=" + endDate + "&page=0&h1=true";
+
+//build ajax call
+$.ajax ({url: queryURL, method: "GET"})
+.done(function(NYTsearch){
+	console.log(response);
+
+
+})
